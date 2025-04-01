4b0b39b78c7de4f9e548e6c6152afcfa29ceb06e
Max Melnichenko <m.y.melnich@gmail.com>
2025-04-01 17:25:53 -0400

    Update

--- run_all_mat_generators.m
+++ run_all_mat_generators.m
@@ -1,6 +1,6 @@
 addpath('matrix_generators/')
 
 figure
-gen_mat_alg971_paper("/test_mat_10k_rank_2k/", 1000, 1000, 200, 49, "generate")
+gen_mat_alg971_paper("/test_mat_10k_rank_2k/", 10000, 10000, 2000, 49, "generate")
 
-gen_mat_rob_paper("/test_mat_rob_paper/", 1000, 1000, 49)
+gen_mat_rob_paper("/test_mat_rob_paper/", 10000, 10000, 49)
